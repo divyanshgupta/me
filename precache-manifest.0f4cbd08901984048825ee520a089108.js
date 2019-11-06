b344c134cb17211715e00eae4aa8a6dcff2a1098
divyansh gupta <divyansh.gupta@iongroup.com>
2019-11-06 17:16:45 +0530

    Updates

---
+++ precache-manifest.0f4cbd08901984048825ee520a089108.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "ac1209e1e8119adc30e3",
+    "url": "/me/static/js/main.ac1209e1.chunk.js"
+  },
+  {
+    "revision": "8553104d46cc6b49d405",
+    "url": "/me/static/js/2.8553104d.chunk.js"
+  },
+  {
+    "revision": "ac1209e1e8119adc30e3",
+    "url": "/me/static/css/main.b52f12e6.chunk.css"
+  },
+  {
+    "revision": "7c6f4c5abd9591ba68a7ad3fe4243081",
+    "url": "/me/index.html"
+  }
+];
